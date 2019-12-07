7ce65c07fc69909f1849ed679f7bf971fd9524de
CNine <cnine229@gmail.com>
2019-12-07 21:18:52 +0800

    feat: welcome come to SPACE

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to the repo" />
+    <HelloWorld msg="Welcome to space" />
   </div>
 </template>
 
