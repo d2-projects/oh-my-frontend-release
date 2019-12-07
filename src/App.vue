8ca809d4952fd0407c39ecc61da0ddb1ab5c461b
CNine <cnine229@gmail.com>
2019-12-07 20:19:56 +0800

    feat: welcome to the repo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Here" />
+    <HelloWorld msg="Welcome to the repo" />
   </div>
 </template>
 
