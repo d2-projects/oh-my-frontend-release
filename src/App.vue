fee1057ed7d23fd11bd578a3d0bef81abe65cf36
CNine <cnine229@gmail.com>
2019-12-07 23:42:37 +0800

    fix: welcome to SPACE station

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to SPACE" />
+    <HelloWorld msg="Welcome to SPACE station" />
   </div>
 </template>
 
