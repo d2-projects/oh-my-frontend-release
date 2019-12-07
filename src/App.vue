15f35b89871fb0b0bd4a6cd8f29daf4122aaf359
CNine <cnine229@gmail.com>
2019-12-07 23:06:22 +0800

    fix(wording): change space to SPACE

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to space" />
+    <HelloWorld msg="Welcome to SPACE" />
   </div>
 </template>
 
