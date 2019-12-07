56ccff245672812ca43bf743a9d3d6f815feb9bc
CNine <cnine229@gmail.com>
2019-12-08 00:55:20 +0800

    fix: welcome to SPACE small station, we are not here

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to SPACE small station, we are here" />
+    <HelloWorld msg="Welcome to SPACE small station, we are not here" />
   </div>
 </template>
 
