82f026fa399270f833c85046c2b568ca2c454435
CNine <cnine229@gmail.com>
2019-12-08 01:09:27 +0800

    feat: welcome to Night

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to SPACE small station, all here" />
+    <HelloWorld msg="Welcome to Night" />
   </div>
 </template>
 
